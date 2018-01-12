91ed0ba09ef9909cb78ce39de76d014584c0778b
Tony Cioara <tonyangelo9707@gmail.com>
2018-01-11 21:46:20 -0800

    Finished search and factorial exercises

--- source/recursion.py
+++ source/recursion.py
@@ -9,14 +9,17 @@ def factorial(n):
     # implement factorial_iterative and factorial_recursive below, then
     # change this to call your implementation to verify it passes all tests
     # return factorial_iterative(n)
-    return factorial_recursive(n)
+    return factorial_iterative(n)
 
 
 def factorial_iterative(n):
     # TODO: implement the factorial function iteratively here
-    pass
+    final_number = 1
+    for current_number in range(1, n + 1):
+        final_number = final_number * current_number
     # once implemented, change factorial (above) to call factorial_iterative
     # to verify that your iterative implementation passes all tests
+    return final_number
 
 
 def factorial_recursive(n):
--- source/search.py
+++ source/search.py
@@ -1,10 +1,11 @@
-#!python
+import random
+
 
 def linear_search(array, item):
     """return the first index of item in array or None if item is not found"""
     # implement linear_search_iterative and linear_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    return linear_search_iterative(array, item)
+    return linear_search_recursive(array, item)
     # return linear_search_recursive(array, item)
 
 
@@ -18,7 +19,11 @@ def linear_search_iterative(array, item):
 
 def linear_search_recursive(array, item, index=0):
     # TODO: implement linear search recursively here
-    pass
+    if array[index] == item:
+        return index
+    if index < len(array) - 1:
+        return linear_search_recursive(array, item, index + 1)
+    return None
     # once implemented, change linear_search to call linear_search_recursive
     # to verify that your recursive implementation passes all tests
 
@@ -27,19 +32,73 @@ def binary_search(array, item):
     """return the index of item in sorted array or None if item is not found"""
     # implement binary_search_iterative and binary_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    return binary_search_iterative(array, item)
+    return binary_search_recursive(array, item)
     # return binary_search_recursive(array, item)
 
 
 def binary_search_iterative(array, item):
     # TODO: implement binary search iteratively here
-    pass
+    first_index = 0
+    last_index = len(array) - 1
+    to_return_none = False
+
+    while last_index - first_index >= 1:
+        if last_index - first_index == 1:
+            to_return_none = True
+        new_index = (last_index + first_index) / 2
+        check_for_2_numbers = False
+        if new_index % 1 != 0:
+            check_for_2_numbers = True
+
+        new_index = int(new_index)
+        print(first_index, " ", last_index)
+        if array[new_index] == item:
+            return new_index
+        elif array[new_index] > item:
+            last_index = new_index
+        elif array[new_index] < item:
+            if check_for_2_numbers is True:
+                if array[new_index + 1] == item:
+                    return new_index + 1
+                first_index = new_index + 1
+            first_index = new_index
+        if to_return_none is True:
+            return None
+    return None
     # once implemented, change binary_search to call binary_search_iterative
     # to verify that your iterative implementation passes all tests
 
 
-def binary_search_recursive(array, item, left=None, right=None):
+def binary_search_recursive(array, item, left=None, right=None, to_return_none=False):
     # TODO: implement binary search recursively here
-    pass
+    if left is None:
+        left = 0
+        right = len(array) - 1
+    if to_return_none is True:
+        return None
+    if right - left == 1:
+        to_return_none = True
+    new_index = (right + left) / 2
+    check_for_2_numbers = False
+    if new_index % 1 != 0:
+        check_for_2_numbers = True
+    new_index = int(new_index)
+    if array[new_index] == item:
+        return new_index
+    elif array[new_index] > item:
+        return binary_search_recursive(array, item, left, new_index, to_return_none)
+    elif array[new_index] < item:
+        if check_for_2_numbers is True:
+            if array[new_index + 1] == item:
+                return new_index + 1
+            return binary_search_recursive(array, item, new_index + 1, right, to_return_none)
+        return binary_search_recursive(array, item, new_index, right, to_return_none)
+    return None
+
     # once implemented, change binary_search to call binary_search_recursive
     # to verify that your recursive implementation passes all tests
+
+
+if __name__ == '__main__':
+    names = ['Alex', 'Brian', 'Julia', 'Kojin', 'Nabil', 'Nick', 'Winnie']
+    print(binary_search_recursive(names, "Nicke"))
